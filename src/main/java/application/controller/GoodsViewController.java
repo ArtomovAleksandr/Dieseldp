3af863d70d9a0e0ae23f7b12f1f79efe6f9f974b
Artomov <diesel_dp@yahoo.com>
2018-10-22 23:02:01 +0300

    create project

---
+++ src/main/java/application/controller/GoodsViewController.java
@@ -0,0 +1,14 @@
+package application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GoodsViewController {
+    @GetMapping()
+    public String index(){
+        return "redirect:/index";
+    }
+
+}
