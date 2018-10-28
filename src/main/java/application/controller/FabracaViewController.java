22ba7e81e70ff323996f73caca0e57f492c39f3a
Artomov <diesel_dp@yahoo.com>
2018-10-28 11:31:49 +0200

    add fabrica.jsp

---
+++ src/main/java/application/controller/FabracaViewController.java
@@ -0,0 +1,20 @@
+package application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/fabrica")
+public class FabracaViewController {
+    @GetMapping("/")
+    public String index(){
+        return "fabrica/fabrica_create";
+    }
+    @GetMapping("/{id}")
+    public String edit(@PathVariable int id, Model model){
+        return "fabrica/fabrica_edit";
+    }
+}
--- src/main/java/application/controller/GoodsViewController.java
+++ src/main/java/application/controller/GoodsViewController.java
@@ -2,13 +2,12 @@ package application.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GoodsViewController {
     @GetMapping("/")
     public String index(){
-        return "index";
+        return "fabrica/fabrica_create";
     }
 
 }
